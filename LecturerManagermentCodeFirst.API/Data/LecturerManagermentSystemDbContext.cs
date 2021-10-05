2110bdc07976d077b721d09650d8fd5760e87d0e
chien1108 <chiendao2000@gmail.com>
2021-10-05 15:53:55 +0700

    Chien add DBcontext

--- LecturerManagermentCodeFirst.API/Data/LecturerManagermentSystemDbContext.cs
+++ LecturerManagermentCodeFirst.API/Data/LecturerManagermentSystemDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LecturerManagermentCodeFirst.API.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,23 @@ namespace LecturerManagermentCodeFirst.API.Data
         {
 
         }
+
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AdvancedLearning> AdvancedLearnings { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<DynamicClassFactor> DynamicClassFactors { get; set; }
+        public DbSet<GraduationThesis> GraduationTheses { get; set; }
+        public DbSet<LecturerScientificResearch> LecturerScientificResearches { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<ScientificResearchGuide> ScientificResearchGuides { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<SubjectType> SubjectTypes { get; set; }
+        public DbSet<Teaching> Teachings { get; set; }
+        public DbSet<TrainingSystem> TrainingSystems { get; set; }
+        public DbSet<SubjectDepartment> SubjectDepartments { get; set; }
+        public DbSet<StandardTime> StandardTimes { get; set; }
+        public DbSet<MachineRoom> MachineRooms { get; set; }
+
     }
 }
