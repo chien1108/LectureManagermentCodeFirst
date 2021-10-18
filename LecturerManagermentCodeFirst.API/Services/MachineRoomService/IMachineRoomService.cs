da454e40faaf2de40d6cfc2b4c5430ca5663804d
Phuong <hp29121997@gmail.com>
2021-10-18 18:28:19 +0700

    troi dat dung hoa

--- LecturerManagermentCodeFirst.API/Services/MachineRoomService/IMachineRoomService.cs
+++ LecturerManagermentCodeFirst.API/Services/MachineRoomService/IMachineRoomService.cs
@@ -1,6 +1,15 @@
-﻿namespace LecturerManagermentCodeFirst.API.Services.MachineRoomService
+﻿using LecturerManagermentCodeFirst.DTO.DTOS.MachineRoom;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LecturerManagermentCodeFirst.API.Services.MachineRoomService
 {
     public interface IMachineRoomService
     {
+        Task<ServiceResponse<GetMachineRoomDto>> GetMachineRoomById(int id);
+        Task<ServiceResponse<IEnumerable<GetMachineRoomDto>>> GetMachineRooms();
+        Task<ServiceResponse<IEnumerable<GetMachineRoomDto>>> AddMachineRoom(AddMachineRoomDto add);
+        Task<ServiceResponse<GetMachineRoomDto>> UpdateMachineRoom(UpdateMachineRoomDto update);
+        Task<ServiceResponse<IEnumerable<GetMachineRoomDto>>> DeleteMachineRoom(int id);
     }
 }
